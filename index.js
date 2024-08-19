f00dcebcdbef93642cb50050bc18dab25ffc9d7f
Brendha Eduarda Rodrigues <136868276+brendhaed@users.noreply.github.com>
2024-08-19 19:29:40 -0300

    exercicios typescript

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
-console.log("hello world")
-
-var nome = "brendha";
-var idade = 20;
-
-var curso="front-end em react",
- topico="javacript basico"
-
-console.log(curso, topico)
-
-const nota1=10
-const nota2=8
-const somaDasNotas = nota1+nota2
-const mediaDasNotas =(nota1+nota2)/2
-console.log("soma das notas:",somaDasNotas)
-console.log("media das notas:", mediaDasNotas)
-
-const numero=16
-const raizDoNumero=Math.sqrt(numero)
-console.log("raiz quadrada de 16:",raizDoNumero)
-
+"use strict";
+const numero1 = 10;
+const numero2 = 3;
+console.log("hello world");
+console.log(numero1 + numero2);
